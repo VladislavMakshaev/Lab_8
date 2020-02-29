bde54dd9a1a7e0e2ba2441cdc4b81985ea1f92e7
VladislavMakshaev <vlad.makshaev@gmail.com>
2020-02-29 13:40:04 +0400

    second

---
+++ src/main/java/ru/ncedu/course/catalog_example/service/CommentService.java
@@ -0,0 +1,58 @@
+package ru.ncedu.course.catalog_example.service;
+
+import ru.ncedu.course.catalog_example.exception.OfferingNotFoundException;
+import ru.ncedu.course.catalog_example.exception.UnauthorizedException;
+import ru.ncedu.course.catalog_example.model.dao.CommentDAO;
+import ru.ncedu.course.catalog_example.model.dao.OfferingDAO;
+import ru.ncedu.course.catalog_example.model.dto.CommentDTO;
+import ru.ncedu.course.catalog_example.model.entity.CommentEntity;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Stateless
+public class CommentService {
+
+    @Inject
+    private AuthorizationBean authorizationBean;
+
+    @Inject
+    private CommentDAO commentDAO;
+
+    @Inject
+    private OfferingDAO offeringDAO;
+
+    public CommentDTO create(String message, long offering) throws UnauthorizedException, OfferingNotFoundException {
+        CommentEntity entity = new CommentEntity();
+        entity.setMessage(message);
+        entity.setOffering(offeringDAO.findById(offering).orElseThrow(OfferingNotFoundException::new));
+        entity.setAuthor(authorizationBean.getUser().orElseThrow(UnauthorizedException::new));
+        commentDAO.create(entity);
+        return new CommentDTO(entity);
+    }
+
+    public List<CommentDTO> findAllByOffering(long offering) {
+        List<CommentEntity> comments = commentDAO.findByOfferingId(offering);
+        List<CommentDTO> result = commentDAO.findByOfferingId(offering).stream().map(CommentDTO::new).collect(Collectors.toList());
+        for (int i=0; i<result.size(); i++) {
+            if (authorizationBean.isAuthorized()) {
+                /*
+                if(comments.get(i).getLikes().contains(authorizationBean.getUser())) {
+                    result.get(i).setLiked(true);
+                }
+                 */
+
+                for (int j = 0; j < comments.get(i).getLikes().size(); j++) {
+                    if (comments.get(i).getLikes().get(j).getId() == authorizationBean.getUser().get().getId()) {
+                        result.get(i).setLiked(true);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/ru/ncedu/course/catalog_example/service/JourneyBean.java
@@ -0,0 +1,44 @@
+package ru.ncedu.course.catalog_example.service;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Stateful
+@SessionScoped
+public class JourneyBean implements Serializable {
+
+    private String url;
+    private List<String> list = new ArrayList<>();
+
+    public JourneyBean() {
+    }
+
+    public void addUrl(String url, String params) {
+        String string = null;
+        if (!url.contains("favicon.ico")) {
+            string = "Opened " + url;
+            if (params != null) {
+                string += "?" + params;
+            }
+            this.list.add(string);
+        }
+
+    }
+
+    public void clear() {this.list.clear();}
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
